44dcd3726f45c760e53caebd8fb024d989094e60
prework <143371945+Yogesh699@users.noreply.github.com>
2023-10-22 22:30:49 +1030

    start timer added

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 let startBtn = document.getElementById("start-btn");
 startBtn.onclick = startSubediQuiz;
+let timeEl = document.getElementById("time");
+let timeLeft = 130;
+let timerId;
 
 function startSubediQuiz(){
-    console.log("quiz started")
+    console.log("quiz started");
+    timerId=setInterval(timerHandler, 1000);
+    timeEl.textContent = timeLeft;
+
+}
+
+function timerHandler(){
+    timeLeft--;
+    timeEl.textContent = timeLeft
 }
