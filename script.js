af9cb825498db40ae4a70b94bc129eb2f5539003
prework <143371945+Yogesh699@users.noreply.github.com>
2023-10-29 21:50:38 +1030

    question added

--- script.js
+++ script.js
@@ -4,6 +4,33 @@ let timeEl = document.getElementById("time");
 let timeLeft = 130;
 let timerId;
 
+let questions = [
+    {
+        question: "q1",
+        answers: [ 'a1', 'a2', 'a3', 'a4'],
+        correctAns :'a3'
+    },
+    {
+        question: "q2",
+        answers: [ 'a1', 'a2', 'a3', 'a4'],
+        correctAns :'a1'
+    },
+    {
+        question: "q3",
+        answers: [ 'a1', 'a2', 'a3', 'a4'],
+        correctAns :'a4'
+    },
+    {
+        question: "q4",
+        answers: [ 'a1', 'a2', 'a3', 'a4'],
+        correctAns :'a2'
+    },
+    {
+        question: "q5",
+        answers: [ 'a1', 'a2', 'a3', 'a4'],
+        correctAns :'a1'
+    },
+]
 function startSubediQuiz(){
     console.log("quiz started");
     timerId=setInterval(timerHandler, 1000);
