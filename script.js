04d6fc3a64e1dbe8745fa44653c33bd1e5a3015b
prework <143371945+Yogesh699@users.noreply.github.com>
2023-10-29 22:09:22 +1030

    more code to javascript

--- script.js
+++ script.js
@@ -7,38 +7,44 @@ let timerId;
 let questions = [
     {
         question: "q1",
-        answers: [ 'a1', 'a2', 'a3', 'a4'],
-        correctAns :'a3'
+        answers: ['a1', 'a2', 'a3', 'a4'],
+        correctAns: 'a3'
     },
     {
         question: "q2",
-        answers: [ 'a1', 'a2', 'a3', 'a4'],
-        correctAns :'a1'
+        answers: ['a1', 'a2', 'a3', 'a4'],
+        correctAns: 'a1'
     },
     {
         question: "q3",
-        answers: [ 'a1', 'a2', 'a3', 'a4'],
-        correctAns :'a4'
+        answers: ['a1', 'a2', 'a3', 'a4'],
+        correctAns: 'a4'
     },
     {
         question: "q4",
-        answers: [ 'a1', 'a2', 'a3', 'a4'],
-        correctAns :'a2'
+        answers: ['a1', 'a2', 'a3', 'a4'],
+        correctAns: 'a2'
     },
     {
         question: "q5",
-        answers: [ 'a1', 'a2', 'a3', 'a4'],
-        correctAns :'a1'
+        answers: ['a1', 'a2', 'a3', 'a4'],
+        correctAns: 'a1'
     },
-]
-function startSubediQuiz(){
-    console.log("quiz started");
-    timerId=setInterval(timerHandler, 1000);
-    timeEl.textContent = timeLeft;
+];
 
+let questionsPage = document.getElementById("questions-page");
+let answers = document.getElementById("answers-page");
+let index = 0;
+let startPage = document.getElementById("start-page");
+
+function startSubediQuiz() {
+    console.log("quiz started");
+    timerId = setInterval(timerHandler, 1000);
+    timeEl.textContent = timeLeft;
+    startPage.setAttribute('class', 'hide-me');
 }
 
-function timerHandler(){
+function timerHandler() {
     timeLeft--;
     timeEl.textContent = timeLeft
 }
